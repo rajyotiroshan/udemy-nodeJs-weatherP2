9ef8ca67556e3f9b73c154cfbc4957ff3a949852
rajan <rajangreedge@gmail.com>
2019-03-05 14:59:31 +0530

    make geocode a local module

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-const request = require("request");
+const geoCode = require("./utils/geocode");
 
 //const url =
   //"https://api.darksky.net/forecast/a3a3eac404d8ba7fb32ffb3c826071e9/37.8267,-122.4233?units=si&lang=es";
-
-const urlMapBox = "https://api.mapbox.com/geocoding/v5/mapbox.places/delhicd.json?access_token=pk.eyJ1IjoicXJhamFuIiwiYSI6ImNqb2xwMzJpdjBnMjEza214dmw1aGs4d24ifQ.JeBKCBGxljAn1DYq_3VzgQ&limit=1";
 /* request(
   {
     url: url,
@@ -24,48 +22,12 @@ const urlMapBox = "https://api.mapbox.com/geocoding/v5/mapbox.places/delhicd.jso
   }
 ); */
 
-//Geocoding
-//Address 
 
-/* request({
-    url: urlMapBox,
-    json: true
-}, (error, response)=>{
-   
-    if(error) {
-      console.log("Unable to connect to server.")
-    }else if(response.body.features.length === 0 ) {
-      console.log("Error not found ::Couldnot find the search term.!");
-    }else {
-      const lat = response.body.features[0].center[0];
-      const long = response.body.features[0].center[1];
-      console.log(lat+","+long);
-    }
-}); */
-
-const geoCode = (address, callback)=>{
-  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=pk.eyJ1IjoicXJhamFuIiwiYSI6ImNqb2xwMzJpdjBnMjEza214dmw1aGs4d24ifQ.JeBKCBGxljAn1DYq_3VzgQ&limit=1`;
-  request({
-    url: url,
-    json: true
-  },(error,response)=>{
-    if(error) {
-      callback('Unable to connect to location services',undefined);
-    }else if(response.body.features.length === 0) {
-      callback(' Unable to find location.Try another search.',undefined);
-    }else {
-      callback(undefined, {
-        latitude: response.body.features[0].center[0],
-        longitude: response.body.features[0].center[1],
-        location: response.body.features[0].place_name
-      });
-    }
-  });
-}
-
-geoCode("burari new delhi", (error, data)=> {
+geoCode("kandiwali", (error, data)=> {
   console.log("Error",error);
   console.log('Date', data);
 });
+
+
 
 
