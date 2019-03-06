ecad9231f6f054d826ca3de3273edb22a4ed9ed2
rajan <rajangreedge@gmail.com>
2019-03-06 18:31:23 +0530

    take location from command line

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 const geoCode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
-geoCode("new Delhi", (error, data)=> {
-  if(error) {
-    return console.log("error");
-  }
-  forecast(data.latitude,data.longitude, (error, forecastData) => {
-    if(error) {
-      return console.log(error);
+//take input from user through command line argument
+location = process.argv[2];
+
+if (!location) {
+  console.log("Please provide a location as third argument.");
+  return;
+} else {
+  geoCode(location, (error, data) => {
+    if (error) {
+      return console.log("error");
     }
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+      if (error) {
+        return console.log(error);
+      }
 
-    console.log(data.location);
-    console.log(forecastData);
-  }) 
-});
-
-
-
+      console.log(data.location);
+      console.log(forecastData);
+    });
+  });
+}
